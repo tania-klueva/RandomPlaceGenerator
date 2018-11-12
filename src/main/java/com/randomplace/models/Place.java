cc5780184c300c5c2dab82a7aae462b0d8176fda
Tania Kliuieva <tania.kliuieva@gmail.com>
2018-11-12 16:08:47 +0200

    Initial commit

---
+++ src/main/java/com/randomplace/models/Place.java
@@ -0,0 +1,11 @@
+package com.randomplace.models;
+
+public class Place {
+
+    private int id;
+    private String name;
+    private String address;
+    private String description;
+    private String specification;
+
+}
